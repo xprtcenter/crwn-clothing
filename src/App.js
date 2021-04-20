3c35feab4e6b4b8ce90d69d2beeba8a0e25c7ea2
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2021-04-20 10:43:56 +0530

    Now Menu item link Url work complete update our Menu item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+	<div>
+		<h1>This is Hats page</h1>
+	</div>
+);
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<HomePage />
+			<div>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop/hats" component={HatsPage} />
+				</Switch>
 			</div>
 		);
 	}
