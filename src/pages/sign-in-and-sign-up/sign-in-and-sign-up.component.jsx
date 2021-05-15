5f7e9cf8e7b24e01aff65513135ea35c9d62d789
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2021-05-15 16:48:04 +0530

    Sign in and Sign out Finish

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import SignIn from "../../component/sign-in/sign-in.component";
+import SignUp from "../../component/sign-up/sign-up.component";
 import "./sign-in-and-sign-up.styles.scss";
 
-const SignInAndSignUpPage = () => <SignIn className="sign-in-and-sign-up" />;
-
+const SignInAndSignUpPage = () => (
+	<div className="sign-in-and-sign-up">
+		<SignIn />
+		<SignUp />
+	</div>
+);
 export default SignInAndSignUpPage;
