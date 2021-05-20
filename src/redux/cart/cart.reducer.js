4c15a8898a1e7f0e4bb07cd31fbb24ca164d5aa7
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2021-05-20 16:46:31 +0530

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,7 @@ import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
 	hidden: true,
-	cartItem: [],
+	cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -16,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.ADD_ITEM:
 			return {
 				...state,
-				cartItem: addItemToCart(state.cartItem, action.payload),
+				cartItems: addItemToCart(state.cartItems, action.payload),
 			};
 		default:
 			return state;
